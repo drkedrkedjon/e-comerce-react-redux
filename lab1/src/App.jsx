f48d909046fb91baca30128535a8a84854d0bbb6
Sasa Savic Perusina <sasa@bubulazy.com>
2024-02-01 12:22:44 +0000

    Footer css finished

--- lab1/src/App.jsx
+++ lab1/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import Header from "./components/header/Header";
 
 export default function App() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
---
+++ lab1/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer-container">
+      <div className="footer-details">
+        <ul>
+          <li>Contacto</li>
+          <li>Email: email@email.com</li>
+          <li>Telefono: +34 222 333 222</li>
+        </ul>
+        <ul>
+          <li>Redes Sociales</li>
+          <li>Facebook</li>
+          <li>Twitter</li>
+          <li>Instagram</li>
+        </ul>
+        <ul>
+          <li>Direccion</li>
+          <li>Calle Lejana 54</li>
+          <li>35212 - LPA</li>
+        </ul>
+      </div>
+      <p>© 2024 Drkedrkedjon</p>
+    </footer>
+  );
+}
