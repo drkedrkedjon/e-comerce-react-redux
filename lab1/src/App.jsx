d21915d1f359e5d168c04e5e041b8ce84de44149
Sasa Savic Perusina <sasa@bubulazy.com>
2024-02-01 12:39:59 +0000

    Promotion component finished

--- lab1/src/App.jsx
+++ lab1/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Footer from "./components/Footer";
+import Promotion from "./components/content/Promotion";
 import Header from "./components/header/Header";
 
 export default function App() {
   return (
     <>
       <Header />
+      <Promotion />
       <Footer />
     </>
   );
---
+++ lab1/src/components/content/Promotion.jsx
@@ -0,0 +1,9 @@
+import "./Promotion.css";
+
+export default function Promotion() {
+  return (
+    <div className="promotion">
+      <p>20% de descuento para nuevos clientes</p>
+    </div>
+  );
+}
