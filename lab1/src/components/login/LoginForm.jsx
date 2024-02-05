3117f90aae1b0f73f8ee441330710fbac51d25aa
Sasa Savic Perusina <sasa@bubulazy.com>
2024-02-05 17:51:14 +0000

    COntext finished, working on Login/Logoff functionality

--- lab1/src/components/login/LoginForm.jsx
+++ lab1/src/components/login/LoginForm.jsx
@@ -1,10 +1,13 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import "./LoginForm.css";
 import { UserContext } from "../../contextos/UserContext";
 
 export default function LoginForm() {
   const { user } = useContext(UserContext);
-  console.log(user.isLogged);
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+  });
 
   return (
     <form className="login-form">
@@ -13,12 +16,16 @@ export default function LoginForm() {
         type="text"
         id="name"
         name="name"
+        value={form.name}
+        onChange={(e) => setForm({ ...form, name: e.target.value })}
       />
       <label htmlFor="email">Email:</label>
       <input
         type="email"
         id="email"
         name="email"
+        value={form.email}
+        onChange={(e) => setForm({ ...form, email: e.target.value })}
       />
       {user.isLogged ? <button>Logout</button> : <button>Login</button>}
       {user.isLogged && <p>{`${user.name}, do you want to end session?`}</p>}
--- lab1/src/contextos/UserContext.jsx
+++ lab1/src/contextos/UserContext.jsx
@@ -1,16 +1,23 @@
 /* eslint-disable react/prop-types */
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const UserContext = createContext();
 
 export default function UserProvider({ children }) {
-  const [user, setUser] = useState({
-    name: "",
-    email: "",
-    isLogged: false,
-    isDarkMode: false,
-    shppingCartItems: [],
-  });
+  const [user, setUser] = useState(
+    () =>
+      JSON.parse(localStorage.getItem("userData")) || {
+        name: "",
+        email: "",
+        isLogged: false,
+        isDarkMode: false,
+        shppingCartItems: [],
+      }
+  );
+
+  useEffect(() => {
+    localStorage.setItem("userData", JSON.stringify(user));
+  }, [user]);
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
