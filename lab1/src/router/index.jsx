2e2499ea68ea21fab1539ef7cc66a1b8577261e6
Sasa Savic Perusina <sasa@bubulazy.com>
2024-02-13 13:03:33 +0000

    Cart and login route dome

--- lab1/src/router/index.jsx
+++ lab1/src/router/index.jsx
@@ -2,12 +2,30 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "../components/layout/Layout";
 import Error from "../components/error/Error";
 import MainContent from "../components/content/MainContent";
+import ShoppingCart from "../components/shopping-card/ShoppingCart";
+import LoginForm from "../components/login/LoginForm";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    children: [{ index: true, element: <MainContent /> }],
+    children: [
+      {
+        path: "/",
+        element: <MainContent />,
+      },
+      {
+        path: "/cart",
+        element: <ShoppingCart />,
+      },
+      {
+        path: "/login",
+        element: <LoginForm />,
+      },
+    ],
   },
-  { path: "*", element: <Error /> },
+  {
+    path: "*",
+    element: <Error />,
+  },
 ]);
