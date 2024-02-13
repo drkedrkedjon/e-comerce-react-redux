8656a8f336b158cfa4ba6fede8d67704fde7f051
Sasa Savic Perusina <sasa@bubulazy.com>
2024-02-13 11:42:24 +0000

    Instalando react router dom & strating configuration

--- lab1/src/main.jsx
+++ lab1/src/main.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./main.css";
 import UserProvider from "./contextos/UserContext.jsx";
-import RutasProvider from "./contextos/RutasContext.jsx";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router/index.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <UserProvider>
-      <RutasProvider>
-        <App />
-      </RutasProvider>
+      <RouterProvider router={router} />
     </UserProvider>
   </React.StrictMode>
 );
