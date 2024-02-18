60852318dff2682d1440748b4cb09ac448783d85
Sasa Savic Perusina <sasa@bubulazy.com>
2024-02-18 20:12:23 +0000

    Fidlleing with MyJsonServer

--- lab1/src/custom-hooks/useProduct.jsx
+++ lab1/src/custom-hooks/useProduct.jsx
@@ -17,7 +17,7 @@ export default function useProduct() {
   // API
   // Obtener data desde API inicialmente http://demo8040877.mockable.io/products
   // const API_URL = "http://localhost:3000/products";
-  const API_URL = "http://demo8040877.mockable.io/products";
+  const API_URL = "https://demo8040877.mockable.io/products";
   useEffect(() => {
     setIsLoading(true);
     const getProducts = async () => {
