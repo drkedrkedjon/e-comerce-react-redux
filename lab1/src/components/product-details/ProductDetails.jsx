3432d87734275ebb550984394aebe1b35f6281a7
Sasa Savic Perusina <sasa@bubulazy.com>
2024-02-21 09:50:20 +0000

    Fixed ProductDetails component fetch

--- lab1/src/components/product-details/ProductDetails.jsx
+++ lab1/src/components/product-details/ProductDetails.jsx
@@ -1,15 +1,17 @@
 import "./ProductDetails.css";
 import { Link, useParams } from "react-router-dom";
-import data from "../../assets/data";
+// import data from "../../assets/data";
 import { UserContext } from "../../contextos/UserContext";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import Loader from "../loader/Loader";
 
 export default function ProductDetails() {
   const { id } = useParams();
   const { user, setUser } = useContext(UserContext);
-
-  const findProduct = data.find((product) => product.id === Number(id));
-  const { title, price, description, image, category } = findProduct;
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   const handleAddToCart = () => {
     setUser({
@@ -17,6 +19,45 @@ export default function ProductDetails() {
       shoppingCartItems: [...user.shoppingCartItems, findProduct.id],
     });
   };
+
+  const API_URL = "http://localhost:3000/products";
+  useEffect(() => {
+    setIsLoading(true);
+    const getProducts = async () => {
+      try {
+        const response = await axios.get(API_URL);
+        setProducts(response.data);
+      } catch (error) {
+        if (error.response && error.response.status === 404) {
+          setError("Error loading products");
+        } else {
+          console.error("Error fetching objects", error);
+        }
+      } finally {
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 1000);
+      }
+    };
+
+    getProducts();
+  }, []);
+
+  console.log(products);
+
+  useEffect(() => {
+    if (error) {
+      alert(error);
+      setError(null);
+    }
+  }, [error]);
+
+  if (isLoading) return <Loader />;
+
+  const findProduct = products.find(
+    (product) => product.id.toString() === id.toString()
+  );
+  const { title, price, description, image, category } = findProduct;
 
   return (
     <main className="product-detail-container">
