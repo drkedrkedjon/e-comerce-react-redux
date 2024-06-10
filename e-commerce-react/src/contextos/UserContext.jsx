29a0ff9d5ec9179e8df4a03a816a65e694275496
Sasha S.P <sasa@bubulazy.com>
2024-06-10 16:45:58 +0100

    Update UserContext.jsx

--- e-commerce-react/src/contextos/UserContext.jsx
+++ e-commerce-react/src/contextos/UserContext.jsx
@@ -15,7 +15,7 @@ export default function UserProvider({ children }) {
         role: "",
       }
   );
-
+ 
   useEffect(() => {
     localStorage.setItem("userData", JSON.stringify(user));
   }, [user]);
